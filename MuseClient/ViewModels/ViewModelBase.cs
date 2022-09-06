db027e2140924624e86eddca83bff328d00d7612
kombuchan <76601670+mihikakrishna@users.noreply.github.com>
2022-09-06 16:08:15 -0700

    Created Muse Client with appropriate naming conventions

---
+++ MuseClient/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace MuseClient.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
