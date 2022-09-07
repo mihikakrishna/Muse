5318bebca968f7153512589d52646b52ae50d0bc
Anish Kondepudi <anish.kondepudi@gmail.com>
2022-09-07 13:40:50 -0700

    Set datacontext to begin app on home page

--- MuseClient/App.axaml.cs
+++ MuseClient/App.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using MuseClient.Stores;
 using MuseClient.ViewModels;
 using MuseClient.Views;
 
@@ -17,9 +18,12 @@ public partial class App : Application
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
+            var navigationStore = new NavigationStore();
+            var mainWindowViewModel = new MainWindowViewModel(navigationStore);
+            
             desktop.MainWindow = new MainWindow
             {
-                DataContext = new MainWindowViewModel(),
+                DataContext = mainWindowViewModel,
             };
         }
 
--- MuseClient/ViewModels/MainWindowViewModel.cs
+++ MuseClient/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,15 @@
+using MuseClient.Stores;
+
 namespace MuseClient.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    private readonly NavigationStore _navigationStore;
+
+    public ViewModelBase CurrentViewModel => _navigationStore.CurrentViewModel;
+
+    public MainWindowViewModel(NavigationStore navigationStore)
+    {
+        _navigationStore = navigationStore;
+    }
 }
