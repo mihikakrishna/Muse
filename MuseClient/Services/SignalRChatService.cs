ef44102c5007acdfa1108a1bba22ef985b81fda6
kombuchan <76601670+mihikakrishna@users.noreply.github.com>
2022-09-08 09:42:06 -0700

    Create SignalR Chat Service

---
+++ MuseClient/Services/SignalRChatService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+using MuseDomain.Models;
+using ReactiveUI;
+
+namespace MuseClient.Services
+{
+    public class SignalRChatService
+    {
+        private readonly HubConnection _connection;
+
+        public event Action<ChatMessage> MessageRecieved;
+        public SignalRChatService(HubConnection connection)
+        {
+            _connection = connection;
+            _connection.On<ChatMessage>("RecieveMessage", (chatMessage) => MessageRecieved?.Invoke(chatMessage));
+        }
+        public async Task Connect()
+        {
+            await _connection.StartAsync();
+        }
+        public async Task SendMessage(ChatMessage message)
+        {
+            await _connection.SendAsync("SendMessage", message);
+        }
+    }
+}
