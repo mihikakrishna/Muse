8e6efb071f0af7b55b366ffa77d6af918d6c763a
kombuchan <76601670+mihikakrishna@users.noreply.github.com>
2022-09-11 22:54:23 -0700

    Add code to extract message from textBox

--- MuseClient/ViewModels/ListenTogetherWindowViewModel.cs
+++ MuseClient/ViewModels/ListenTogetherWindowViewModel.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.ObjectModel;
+using System.Data;
 using System.Windows.Input;
 using Microsoft.AspNetCore.SignalR.Client;
 using MuseClient.Commands;
@@ -13,10 +14,12 @@ public class ListenTogetherWindowViewModel : ViewModelBase
 {
     private string _errorMessage;
     private bool _isConnected;
+    private string _chatInput;
     public ObservableCollection<string> Messages { get; }
     public ObservableCollection<string> Songs { get; }
     public ICommand SendChatMessageCommand { get; }
     public ICommand NavigateToHomeWindowCommand { get; }
+    
 
     public static ListenTogetherWindowViewModel CreateConnectedViewModel(NavigationStore navigationStore)
     {
@@ -56,10 +59,15 @@ public class ListenTogetherWindowViewModel : ViewModelBase
         
         chatService.MessageReceived += ChatService_MessageReceived;
     }
+    public string ChatInput
+    {
+        get => _chatInput;
+        set => this.RaiseAndSetIfChanged(ref _chatInput, value);
+    }
     private void ChatService_MessageReceived(ChatMessage chatMessage)
     {
-        Messages.Add(chatMessage.Message);
-        Console.WriteLine(chatMessage.Message);
+        Messages.Add(ChatInput);
+        Console.WriteLine(ChatInput);
     }
 
     public string ErrorMessage
