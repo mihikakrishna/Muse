d97d316817601bff51f3889ed08e5cdb952e29e1
Anish Kondepudi <anish.kondepudi@gmail.com>
2022-09-14 01:01:52 -0700

    Move commands for HomePageViewModel into external file

--- MuseClient/ViewModels/HomeWindowViewModel.cs
+++ MuseClient/ViewModels/HomeWindowViewModel.cs
@@ -1,4 +1,5 @@
-using System;
+using System.Windows.Input;
+using MuseClient.Commands;
 using MuseClient.Stores;
 using ReactiveUI;
 
@@ -6,15 +7,19 @@ namespace MuseClient.ViewModels;
 
 public class HomeWindowViewModel : ViewModelBase
 {
-    private readonly NavigationStore _navigationStore;
     private string _username;
     private string _roomCode;
+    
+    public ICommand CreateRoomCommand { get; }
+    public ICommand JoinRoomCommand { get; }
 
     public HomeWindowViewModel(NavigationStore navigationStore)
     {
-        _navigationStore = navigationStore;
         _username = string.Empty;
         _roomCode = string.Empty;
+
+        CreateRoomCommand = new CreateRoomCommand(this, navigationStore);
+        JoinRoomCommand = new JoinRoomCommand(this, navigationStore);
     }
 
     public string Username
@@ -28,23 +33,5 @@ public class HomeWindowViewModel : ViewModelBase
         get => _roomCode;
         set => this.RaiseAndSetIfChanged(ref _roomCode, value);
     }
-
-    public void CreateRoomCommand()
-    {
-        // send roomId to server to createRoom, save result in _roomCode and continue
-        _roomCode = "1234"; // since server logic isnt there yet to generate our room code - hardcoding "1234" for now
-        _navigationStore.CurrentViewModel = ListenTogetherWindowViewModel.CreateConnectedViewModel(
-            navigationStore: _navigationStore, 
-            username: _username, 
-            roomCode: _roomCode);
-    }
-
-    public void JoinRoomCommand()
-    {
-        // send roomId to server to ValidateRoom, if valid then continue, else throw an error window
-        _navigationStore.CurrentViewModel = ListenTogetherWindowViewModel.CreateConnectedViewModel(
-            navigationStore: _navigationStore, 
-            username: _username, 
-            roomCode: _roomCode);
-    }
+    
 }
